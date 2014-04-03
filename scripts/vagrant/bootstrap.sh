c0d61d1fd2ede902778c6dccdd9340a156567a19
Pantazis Deligiannis <pdeligia@me.com>
2014-04-04 00:00:13 +0100

    vagrant fix

--- scripts/vagrant/bootstrap.sh
+++ scripts/vagrant/bootstrap.sh
@@ -4,29 +4,39 @@ echo Getting updates ...
 
 sudo apt-get -y update
 sudo apt-get install -y python-software-properties python g++
-sudo apt-get install -y automake make cmake
+sudo apt-get install -y automake make
 sudo apt-get install -y wget git subversion
-# sudo apt-get install -y mono-complete mono-xbuild
-# sudo add-apt-repository ppa:keks9n/monodevelop-latest
-# sudo apt-get update
-# sudo apt-get install -y monodevelop-latest
 
 export PROJECT_ROOT=/vagrant
 export BUILD_ROOT=/vagrant/build
+export CMAKE_VERSION=2.8.8
 export MONO_VERSION=3.0.7
 export LLVM_RELEASE=34
 
-echo Getting MONO ...
-
+mkdir -p ${BUILD_ROOT}
 cd ${BUILD_ROOT}
+
+echo Getting CMAKE ${CMAKE_VERSION} ...
+
+http://www.cmake.org/files/v2.8/cmake-${CMAKE_VERSION}-Linux-i386.tar.gz
+
+echo Unpacking CMAKE ${CMAKE_VERSION} ...
+
+tar jxf cmake-${CMAKE_VERSION}-Linux-i386.tar.gz
+rm cmake-${CMAKE_VERSION}-Linux-i386.tar.gz
+
+export PATH=${BUILD_ROOT}/cmake-${CMAKE_VERSION}-Linux-i386/bin:$PATH
+
+echo Getting MONO ${MONO_VERSION} ...
+
 wget http://download.mono-project.com/sources/mono/mono-${MONO_VERSION}.tar.bz2
 
-echo Unpacking MONO ...
+echo Unpacking MONO ${MONO_VERSION} ...
 
 tar jxf mono-${MONO_VERSION}.tar.bz2
 rm mono-${MONO_VERSION}.tar.bz2
 
-echo Building MONO ...
+echo Building MONO ${MONO_VERSION} ...
 
 cd ${BUILD_ROOT}/mono-${MONO_VERSION}
 ./configure --prefix=${BUILD_ROOT}/local --with-large-heap=yes --enable-nls=no
@@ -35,7 +45,7 @@ make install
 
 export PATH=${BUILD_ROOT}/local/bin:$PATH
 
-echo Getting LLVM ...
+echo Getting LLVM ${LLVM_RELEASE} ...
 
 mkdir -p ${BUILD_ROOT}/llvm_and_clang
 cd ${BUILD_ROOT}/llvm_and_clang
@@ -45,7 +55,7 @@ svn co http://llvm.org/svn/llvm-project/cfe/branches/release_${LLVM_RELEASE} cla
 cd ${BUILD_ROOT}/llvm_and_clang/src/projects
 svn co http://llvm.org/svn/llvm-project/compiler-rt/branches/release_${LLVM_RELEASE} compiler-rt
 
-echo Building LLVM ...
+echo Building LLVM ${LLVM_RELEASE} ...
 
 mkdir -p ${BUILD_ROOT}/llvm_and_clang/build
 cd ${BUILD_ROOT}/llvm_and_clang/build
@@ -80,6 +90,6 @@ xbuild /p:TargetFrameworkProfile="" /p:Configuration=Debug whoop.sln
 echo Finalising build ...
 
 mv findtools.py findtools-backup.py
-cp ${BUILD_ROOT}/findtools.py findtools.py
+cp ${PROJECT_ROOT}/scripts/vagrant/findtools.py findtools.py
 
 echo Done ...
