39641d1e42c7c7234806253e902347b8d7e90bf3
Pantazis Deligiannis <pdeligia@me.com>
2014-04-05 09:48:25 +0100

    update to vagrant provisioning

--- Scripts/Vagrant/bootstrap.sh
+++ Scripts/Vagrant/bootstrap.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-echo Getting updates ...
+echo $'\n==================='
+echo $'Getting updates ...'
+echo $'===================\n'
 
 sudo apt-get -y update
 sudo apt-get install -y g++
 sudo apt-get install -y make
 sudo apt-get install -y python-software-properties python
 sudo apt-get install -y automake autoconf
-sudo apt-get install -y libtool
+sudo apt-get install -y libtool libgmp
 sudo apt-get install -y wget git subversion mercurial
+sudo apt-get install -y gettext zlib1g-dev asciidoc libcurl4-openssl-dev 
 
 export PROJECT_ROOT=/vagrant
 export BUILD_ROOT=/home/vagrant/whoop
@@ -18,38 +21,66 @@ export MONO_VERSION=3.0.7
 export LLVM_RELEASE=release_34
 
 mkdir -p ${BUILD_ROOT}
+
+echo $'\n======================'
+echo $'Getting latest git ...'
+echo $'======================\n'
+
 cd ${BUILD_ROOT}
+git clone https://github.com/git/git.git 
 
-echo Getting CMAKE ${CMAKE_VERSION} ...
+echo $'\n======================='
+echo $'Building latest git ...'
+echo $'=======================\n'
 
+cd git
+make configure 
+./configure --prefix=/usr
+make all doc
+sudo make install install-doc install-html 
+
+echo $'\n================='
+echo $'Getting CMAKE ...'
+echo $'=================\n'
+
+cd ${BUILD_ROOT}
 wget http://www.cmake.org/files/v2.8/cmake-${CMAKE_VERSION}-Linux-i386.tar.gz
 
-echo Unpacking CMAKE ${CMAKE_VERSION} ...
+echo $'\n==================='
+echo $'Unpacking CMAKE ...'
+echo $'===================\n'
 
 tar zxvf cmake-${CMAKE_VERSION}-Linux-i386.tar.gz
 rm cmake-${CMAKE_VERSION}-Linux-i386.tar.gz
-
 export PATH=${BUILD_ROOT}/cmake-${CMAKE_VERSION}-Linux-i386/bin:$PATH
 
-echo Getting MONO ${MONO_VERSION} ...
+echo $'\n================'
+echo $'Getting MONO ...'
+echo $'================\n'
 
+cd ${BUILD_ROOT}
 wget http://download.mono-project.com/sources/mono/mono-${MONO_VERSION}.tar.bz2
 
-echo Unpacking MONO ${MONO_VERSION} ...
+echo $'\n=================='
+echo $'Unpacking MONO ...'
+echo $'==================\n'
 
 tar jxf mono-${MONO_VERSION}.tar.bz2
 rm mono-${MONO_VERSION}.tar.bz2
 
-echo Building MONO ${MONO_VERSION} ...
+echo $'\n================='
+echo $'Building MONO ...'
+echo $'=================\n'
 
 cd ${BUILD_ROOT}/mono-${MONO_VERSION}
 ./configure --prefix=${BUILD_ROOT}/local --with-large-heap=yes --enable-nls=no
 make -j4
 make install
-
 export PATH=${BUILD_ROOT}/local/bin:$PATH
 
-echo Getting LLVM ${LLVM_RELEASE} ...
+echo $'\n================'
+echo $'Getting LLVM ...'
+echo $'================\n'
 
 mkdir -p ${BUILD_ROOT}/llvm_and_clang
 cd ${BUILD_ROOT}/llvm_and_clang
@@ -68,19 +99,25 @@ cd ${BUILD_ROOT}/llvm_and_clang/src/projects/compiler-rt
 git checkout release_34
 # svn co http://llvm.org/svn/llvm-project/compiler-rt/branches/release_${LLVM_RELEASE} compiler-rt
 
-echo Building LLVM ${LLVM_RELEASE} ...
+echo $'\n================='
+echo $'Building LLVM ...'
+echo $'=================\n'
 
 mkdir -p ${BUILD_ROOT}/llvm_and_clang/build
 cd ${BUILD_ROOT}/llvm_and_clang/build
 cmake -D CMAKE_BUILD_TYPE=Release ../src
 make -j4
 
-echo Getting SMACK ...
+echo $'\n================='
+echo $'Getting SMACK ...'
+echo $'=================\n'
 
 cd ${BUILD_ROOT}
 git clone https://github.com/smackers/smack.git ${BUILD_ROOT}/smack/src
 
-echo Building SMACK ...
+echo $'\n=================='
+echo $'Building SMACK ...'
+echo $'==================\n'
 
 mkdir -p ${BUILD_ROOT}/smack/build
 cd ${BUILD_ROOT}/smack/build
@@ -88,12 +125,16 @@ cmake -D LLVM_CONFIG=${BUILD_ROOT}/llvm_and_clang/build/bin -D CMAKE_BUILD_TYPE=
 make -j4
 make install
 
-echo Getting Z3 ...
+echo $'\n=============='
+echo $'Getting Z3 ...'
+echo $'==============\n'
 
 cd ${BUILD_ROOT}
 git clone https://git01.codeplex.com/z3
 
-echo Building Z3 ...
+echo $'\n==============='
+echo $'Building Z3 ...'
+echo $'===============\n'
 
 cd ${BUILD_ROOT}/z3
 autoconf
@@ -103,12 +144,16 @@ cd build
 make -j4
 ln -s z3 z3.exe
 
-echo Getting CVC4 ...
+echo $'\n================'
+echo $'Getting CVC4 ...'
+echo $'================\n'
 
 cd ${BUILD_ROOT}
 git clone https://github.com/CVC4/CVC4.git ${BUILD_ROOT}/CVC4/src
 
-echo Building CVC4 ...
+echo $'\n================='
+echo $'Building CVC4 ...'
+echo $'=================\n'
 
 cd ${BUILD_ROOT}/CVC4/src
 MACHINE_TYPE="x86_64" contrib/get-antlr-3.4
@@ -123,23 +168,36 @@ make install
 cd ${BUILD_ROOT}/CVC4/install/bin
 ln -s cvc4 cvc4.exe
 
-echo Building CHAUFFEUR ...
+echo $'\n================='
+echo $'Getting WHOOP ...'
+echo $'=================\n'
 
+cd ${BUILD_ROOT}
 cp -a ${PROJECT_ROOT}/. ${BUILD_ROOT}/whoop/
+
+echo $'\n======================'
+echo $'Building CHAUFFEUR ...'
+echo $'======================\n'
+
 cd ${BUILD_ROOT}/whoop
 mkdir -p ${BUILD_ROOT}/whoop/FrontEndPlugin/build
 cd ${BUILD_ROOT}/whoop/FrontEndPlugin/build
 cmake -D LLVM_CONFIG=${BUILD_ROOT}/llvm_and_clang/build/bin -D CMAKE_BUILD_TYPE=Release ../src
 make -j4
 
-echo Building WHOOP ...
+echo $'\n=================='
+echo $'Building WHOOP ...'
+echo $'==================\n'
 
-cp -a ${PROJECT_ROOT}/. ${BUILD_ROOT}/whoop/
 cd ${BUILD_ROOT}/whoop
 xbuild /p:TargetFrameworkProfile="" /p:Configuration=Debug whoop.sln
 
-echo Configuring WHOOP ...
+echo $'\n====================='
+echo $'Configuring WHOOP ...'
+echo $'=====================\n'
 
 cp /Scripts/Vagrant/findtools.vagrant.py findtools.py
 
-echo Done ...
+echo $'\n========'
+echo $'Done ...'
+echo $'========\n'
