1d6680377a040f6053afc24d7a9cd2c99a54932c
Pantazis Deligiannis <pdeligia@me.com>
2014-05-20 10:38:50 +0530

    new test

--- testsuite/locks_with_if/case2/test.c
+++ testsuite/locks_with_if/case2/test.c
@@ -1,4 +1,4 @@
-//xfail:DRIVER_ERROR
+//pass
 //
 
 #include <linux/device.h>
@@ -7,7 +7,6 @@
 struct shared {
 	int resource;
 	struct mutex mutex;
-	struct mutex mutex2;
 };
 
 static void entrypoint1(struct test_device *dev)
@@ -15,15 +14,9 @@ static void entrypoint1(struct test_device *dev)
 	struct shared *tp = testdev_priv(dev);
 	
 	mutex_lock(&tp->mutex);
-	tp->resource = 1;
-}
-
-static void entrypoint2(struct test_device *dev)
-{
-	struct shared *tp = testdev_priv(dev);
-	
-	mutex_lock(&tp->mutex2);
-	tp->resource = 2;
+	int x = (int)tp->resource;
+	if (x == 5)
+		tp->resource = 3;
 	mutex_unlock(&tp->mutex);
 }
 
@@ -34,16 +27,13 @@ static int init(struct pci_dev *pdev, const struct pci_device_id *ent)
 	
 	tp = testdev_priv(dev);
 	mutex_init(&tp->mutex);
-	mutex_init(&tp->mutex2);
 	
 	entrypoint1(dev);
-	entrypoint2(dev);
 	
 	return 0;
 }
 
 static struct test_driver test = {
 	.probe = init,
-	.ep1 = entrypoint1,
-	.ep2 = entrypoint2
+	.ep1 = entrypoint1
 };
